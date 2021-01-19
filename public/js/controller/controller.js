7f1a3465a69e4ab5ac635df3bbf9768d8f5066e2
unknown <fedenkoolena@gmail.com>
2021-01-19 16:14:57 +0200

    jjj

--- public/js/controller/controller.js
+++ public/js/controller/controller.js
@@ -3,29 +3,32 @@ export default class Controller {
   constructor(view, model) {
     this.view = view;
     this.model = model;
+    this.getWeather = this.getWeather.bind(this);
+    this.cngWeatherCard = this.cngWeatherCard.bind(this);
+    this.delWeatherCard = this.delWeatherCard.bind(this);
+    this.clearCity = this.clearCity.bind(this);
   }
 
-  getWeather = () => {
+  getWeather() {
     let city = this.view.mainInput.value;
     this.view.mainInput.value = "";
     this.model.getWeather(city);
   }
 
-  delWeatherCard = (event) => {
+  delWeatherCard(event) {
     this.model.delCity(event.target.value);
   }
 
-  cngWeatherCard = (event) => {
+  cngWeatherCard(event) {
     this.model.editCity(event.target.value);
   }
 
-  clearCity = () => {
+  clearCity() {
     this.model.clearCity();
   }
-
   addHandle() {
-    this.view.mainButton.addEventListxener("click", this.getWeather);
-    this.view.clearButton.addEventListener("click", this.clearCity);cc
+    this.view.mainButton.addEventListener("click", this.getWeather);
+    this.view.clearButton.addEventListener("click", this.clearCity);
     $("#app").on("click", ".del", this.delWeatherCard);
   }
 }
